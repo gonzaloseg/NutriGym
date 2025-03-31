a452cebbd62e11bbbdef5e141606e1b51d4b90e1
gonzaloseg <gonzaloseg05@gmail.com>
2025-03-31 09:55:48 +0200

    que pasa

--- src/app/pages/suplementos/suplementos.component.ts
+++ src/app/pages/suplementos/suplementos.component.ts
@@ -7,7 +7,7 @@ import { ApiService } from '../../services/api_rutina/api_rutina.service';
   styleUrls: ['./suplementos.component.css']
 })
 export class SuplementosComponent implements OnInit {
-  searchQuery: string = '';
+  /*searchQuery: string = '';
   recipes: any;
 
   constructor(private apiService: ApiService) {}
@@ -23,5 +23,23 @@ export class SuplementosComponent implements OnInit {
       this.recipes = response;
       console.log(this.recipes);
     });
+  }*/
+    searchQuery: string = '';
+    recipes: any[] = [];
+  
+    constructor(private apiService: ApiService) {}
+  
+    ngOnInit() {
+      this.buscarRecetas('chicken'); // Carga inicial
+    }
+  
+    buscarRecetas(query: string) {
+      if (!query.trim()) return;
+    
+      this.apiService.getRecipes(query).subscribe(response => {
+        this.recipes = response.hits; // Extraemos solo el array de recetas
+        console.log(this.recipes);
+      });
+    }
   }
-}
+
--- src/app/services/api_rutina/api_rutina.service.ts
+++ src/app/services/api_rutina/api_rutina.service.ts
@@ -1,16 +1,34 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  private apiKey = '8e9ba524827343a0829d9b5e67aa27fd'; // Clave de la API
+ /* private apiKey = '8e9ba524827343a0829d9b5e67aa27fd'; // Clave de la API
   private apiUrl = 'https://api.spoonacular.com/recipes/complexSearch'; // URL de la API
 
   constructor(private http: HttpClient) {}
 
   getRecipes(query: string) {
     return this.http.get(`${this.apiUrl}?query=${query}&apiKey=${this.apiKey}`);
+  }*/
+    private apiUrl = 'https://api.edamam.com/api/recipes/v2';
+  private appId = 'f4d91107'; // Reemplaza con tu App ID
+  private appKey = 'c492e9c58faf44f8cb4e5bfb81507bf5'; // Reemplaza con tu App Key
+  private userId = 'f4d91107'; // Reemplaza con tu User ID real
+
+  constructor(private http: HttpClient) {}
+
+  getRecipes(query: string): Observable<any> {
+    const headers = new HttpHeaders({
+      'f4d91107': this.userId // Agregar el userID en los headers
+    });
+
+    const url = `${this.apiUrl}?type=public&q=${query}&app_id=${this.appId}&app_key=${this.appKey}`;
+    
+    return this.http.get(url, { headers });
   }
 }
